13d46c786088fe95cdfbfc75f80d66434d828f29
D.G. <Wingman4l7@gmail.com>
2013-01-26 00:13:25 +1100

    v1.0 release
    Fixes hidden border issue by using inner borders, only puts border on
    images within the unordered list of album covers.  Added version number
    into the header.

--- What.CD Bad Covers Collage Edition.user.js
+++ What.CD Bad Covers Collage Edition.user.js
@@ -1,28 +1,40 @@
 // ==UserScript==
 // @name           What.CD Bad Covers Collage Edition
-// @namespace      google.com
-// @description    Highlights all album covers in a collage not on a good host.
+// @namespace      https://github.com/Wingman4l7/badhost
+// @description    Highlights all album covers in a collage not on a whitelisted host.
 // @include        http*://*what.cd/collages.php?id=*
+// @version        1.0
 // ==/UserScript==
 
-//By death2y
-var goodhosts = [ //If you want more hosts to be "good", add them to this list as a regex matching their domain
+//To whitelist a host, add them to this list as a regex matching their domain
+var goodhosts = [ 
     /whatimg\.com/
 ];
 
-/******************************************************************************/
-if (!window.location.search.match(/Report collage/)) { //Collage description page
-    var coverElemNodeList = document.getElementsByTagName("img");
-	var coverURL;
-	for (var i = 0; i < coverElemNodeList.length; i++) {
-		coverURL = coverElemNodeList[i].src;
-		for (var j = 0; j < goodhosts.length; j++) {
-			if (coverURL.match(goodhosts[j])) {
-				j = goodhosts.length;
-				coverElemNodeList[i].style.border = ".3em outset green";
+if (!window.location.search.match(/Report collage/)) { // Collage description page
+    
+	// this "clicks" on all the page links, which call collageShow(), 
+	// which loads that page of albums into the page source
+	var pageSpans = document.getElementsByTagName("span");
+	for (var i = 0; i < pageSpans.length; i++) {
+		if(pageSpans[i].id.indexOf("pagelink") != -1) {
+			pageSpans[i].firstChild.click(); 
 			}
-			else {
-				coverElemNodeList[i].style.border = ".3em outset red";
+	}
+	document.getElementById("pagelink0").firstChild.click(); // return to first page
+	
+	// get the images within the UL only
+	var coverElementNodeList = document.getElementById("coverart").querySelectorAll("[class^='image_group_']");
+	var imgElement;
+	for (var i = 0; i < coverElementNodeList.length; i++) {
+		imgElement = coverElementNodeList[i].getElementsByTagName('img')[0];
+		for (var j = 0; j < goodhosts.length; j++) {
+			if (imgElement.src.match(goodhosts[j])) {
+				j = goodhosts.length;
+			}
+			else { 
+				imgElement.style.boxSizing = "border-box"; // inner border
+				imgElement.style.border = ".3em solid red";		
 			}
 		}
 	}
